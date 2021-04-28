2a1a145afd7d055afc0f7b86cd64014dd34b4d6f
Briland Mathieu <briland.mathieu@gmail.com>
2021-04-28 21:33:53 +0200

    tileset add
    tileset

---
+++ assets/tileset/recolored.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="recolored" tilewidth="16" tileheight="16" tilecount="280" columns="10">
+ <image source="Downloads/recolored16x16_Jerom_CC-BY-SA-3.0.png" width="160" height="448"/>
+</tileset>
